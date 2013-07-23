d142d09a511a5a4c0a902ef3b7ac46c834d66134
zhangmeteor <438114643@qq.com>
2013-07-24 07:30:58 +0800

    7.24 morning

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -25,8 +25,8 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     @cart = current_cart
-    product = Product.find ( params[:product_id]  )
-    @line_item = @cart.line_items.build(:product => product)
+    product = Product.find (params[:product_id])
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       if @line_item.save
