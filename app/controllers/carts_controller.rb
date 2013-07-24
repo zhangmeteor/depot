995789fbe7b574795651a7ed7cda3b54d4477614
zhangmeteor <438114643@qq.com>
2013-07-25 02:31:51 +0800

    7.25 morning

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,6 +10,17 @@ class CartsController < ApplicationController
   # GET /carts/1
   # GET /carts/1.json
   def show
+    begin
+      @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid car #{params[:id]}"
+      redirect_to store_url, :notice => 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json {render json:@cart }
+      end
+    end
   end
 
   # GET /carts/new
@@ -54,17 +65,29 @@ class CartsController < ApplicationController
   # DELETE /carts/1
   # DELETE /carts/1.json
   def destroy
+    @cart = current_cart
     @cart.destroy
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url }
-      format.json { head :no_content }
+      format.html { redirect_to (store_url) }
+      format.json { head :no_content}
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_cart
+    begin
       @cart = Cart.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error "Attempt to access invalid car #{params[:id]}"
+      redirect_to store_url, :notice => 'Invalid cart'
+    else
+      respond_to do |format|
+        format.html # show.html.erb
+        format.json {render json:@cart }
+      end
+    end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
