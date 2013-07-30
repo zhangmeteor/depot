0bb7de3255771e71cbda98e068e4a47b5e64ec2d
zhangmeteor <438114643@qq.com>
2013-07-31 02:36:25 +0800

    Mails No error

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default from: 'Zhang Ruby <zhang438114643@gmail.com>'
+  default from: "zhang438114643@gmail.com"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,13 +2,12 @@ Depot::Application.configure do
   #email
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_setting = {
-    :address         => "smtp.gmail.com",
+  config.action_mailer.smtp_settings = {
+    :address         => 'smtp.gmail.com',
     :port            => 587,
-    :domain          => "domain.of.sender.net",
-    :authentication  => "plain",
-    :user_name       => "dave",
-    :password        => "secret",
+    :authentication  => 'plain',
+    :user_name       => 'zhang438114643@gmail.com',
+    :password        => ENV['haohaotiger'],
     :enable_starttls_auto  => true
   }
 
